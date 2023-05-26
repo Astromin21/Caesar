a98046dc1e356dbcb52ce163b201f1f9a8b4d5d1
Astromin21 <docgit@gmail.com>
2023-05-26 11:24:46 +0200

    abfrage auf max 26 zahlen erstellt

--- main.js
+++ main.js
@@ -1,29 +1,88 @@
-
-
+"use strict";
+//-----------------------------Eventlisten---------------------------------
 function abfrage(){
     let text = "";
     let nr = 0;
     
     let wort = document.querySelector('#wort');
-    wort.addEventListener('blur', function() {
-        text = wort.value; 
-        schlussel(text,0)  
-    });
-
     let zahl = document.querySelector('#zahl');
+    wort.addEventListener('blur', function() {
+        text = wort.value;
+        schlussel(text,0);
+
+    })
     zahl.addEventListener('blur', function(){
         nr = zahl.value;
+        nr % 26 === 0 ? (nr = 26) : (nr = nr % 26)
         schlussel(text, +nr);
-    });
+    })
+    
 }
+//------------------------------Main-----------------------------------------
 function schlussel(text, nr) {
     let ausgabe = document.querySelector('#ausgabe');
-    code = text.charCodeAt();
-    console.log(code);
-    ausgabe.innerHTML = text;
-    ergebnis = code + nr;
-    neu = String.fromCharCode(ergebnis)
-    ausgabe.innerHTML = neu;
+    let mySplit = text.split("");
+    let neu = umwandeln(mySplit);
+
+    let code = grossKlein(neu, nr);
+    let ergebnis = ruckwandeln(code);
+    ergebnis = ergebnis.join("");
+    console.log(ergebnis);
+    ausgabe.innerHTML = ergebnis;
+
 }
+//-----------------------------Splitten---------------------------------------
+function umwandeln(mySplit){
+    let neu = [];
+    for(let i = 0; i < mySplit.length; i++){
+        neu.push(parseInt(mySplit[i].charCodeAt()));
+    }
+    return neu;
+}
+//-----------------------------Groß/Klein Abfrage---------------------------------------
+function grossKlein(liste, nr){
+    let checked = []
+    for(let j = 0; j < liste.length; j++){
+        if(liste[j] >= 97 && liste[j] <= 122){
+            let zahl = liste[j] + nr;
+            checked.push(rangeKlein(zahl));
+
+        } else if(liste[j] >= 65 && liste[j] <= 90){
+            let zahl = liste[j] + nr;
+            checked.push(rangeGross(zahl));
+
+        }
+    }
+    return checked;
+}
+//-----------------------------Range Abfrage---------------------------------------
+function rangeKlein(zahl){   
+    if(zahl < 97){
+        zahl += 26;
+    } else if(zahl > 122){
+        zahl -= 26;
+    }
+    return zahl;
+
+}
+function rangeGross(zahl){   
+    if(zahl < 65){
+        zahl += 26;
+    } else if(zahl > 90){
+        zahl = zahl - 26;
+        }
+
+    return zahl;
+}
+
+function ruckwandeln(code) {
+    let ergebnis = [];
+    for( let i = 0; i < code.length; i++) {
+        ergebnis.push(String.fromCharCode(code[i]))
+    }
+    return ergebnis;
+}
+
+
 
 abfrage()
